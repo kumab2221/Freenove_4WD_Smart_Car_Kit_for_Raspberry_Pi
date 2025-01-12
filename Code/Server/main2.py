c74d849e2ca55c2f70ff6573a3e2953a60c8c7a0
Yasuhiro <nukushina.yas@mazda-EandT.co.jp>
2025-01-13 00:43:56 +0900

    main処理を追加

---
+++ Code/Server/main2.py
@@ -0,0 +1,61 @@
+# main2.py
+import time
+from Motor import Motor
+from Buzzer import Buzzer
+from Ultrasonic import Ultrasonic
+
+from yamagata.DrvCtrl import DrvCtrl
+from matsumoto.AreaJdg import AreaJdg
+from matsumoto.BuzrCtrl import BuzzerCtrl
+
+def main2():
+    print("Start")
+    motor= Motor()
+    buzzer = Buzzer()
+    ultra = Ultrasonic()
+    try:
+        area_jdg = AreaJdg()
+        buzeer_ctrl = BuzzerCtrl()
+        motor.setMotorModel(0,0,0,0)
+        
+        print("time, area_state, drv_ctrl_st, fr_drv_out, fl_drv_out, rr_drv_out, rl_drv_out, ")
+        while True:
+            start_time = time.time()
+
+            ### (認知)センサ-Start
+            ulsnc_sig = ultra.get_distance()
+            ### (認知)センサ-End
+
+            ### (判断)制御アプリ-Start
+            # エリア判定
+            area_state = _area_jdg.AreaJdg(ulsnc_sig)
+            # 駆動マネージャー
+            drv_ctrl_st, fr_drv_out, fl_drv_out, rr_drv_out, rl_drv_out = DrvCtrl(area_state)
+            # ブザー判断
+            buzzer_out = _buzeer_ctrl.BuzzerCtrl( area_state, drv_ctrl_st)
+            ### (判断)制御アプリ-End
+
+            ### (制御)デバイス制御-Start
+            # モーター
+            motor.setMotorModel(fl_drv_out, rl_drv_out, fr_drv_out, rr_drv_out)
+            # ブザー
+            buzzer.run(str(buzzer_out))
+            ### (制御)デバイス制御-End
+
+            # 処理周期を10msに保つ
+            elapsed_time = time.time() - start_time
+            sleep_time = max(0, 0.01 - elapsed_time) 
+            time.sleep(sleep_time)
+
+            # 経過時間測定ログをデバックできるように表示する            
+            formatted_time = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(start_time))
+            milliseconds = int((start_time % 1) * 1000)
+            # "time, area_state, drv_ctrl_st, fr_drv_out, fl_drv_out, rr_drv_out, rl_drv_out "
+            print(f"{formatted_time}.{milliseconds:03d}, {area_state}, {drv_ctrl_st}, {fr_drv_out}, {fl_drv_out}, {rr_drv_out}, {rl_drv_out}")
+
+    except KeyboardInterrupt:
+        motor.setMotorModel(0,0,0,0) # モーターを停止する
+        print("Break Loop")
+
+if __name__ == "__main__":
+    main2()
