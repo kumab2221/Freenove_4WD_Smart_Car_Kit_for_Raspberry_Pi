eb5f5f84c627cac97d0efb113340fa7e9bda55bc
Yasuhiro <nukushina.yas@mazda-EandT.co.jp>
2025-01-22 10:22:54 +0900

    AreaJdgから距離情報を出力
    現時点でフィルタ機能はなく、Gatewayしているだけ

--- Code/Server/matsumoto/AreaJdg.py
+++ Code/Server/matsumoto/AreaJdg.py
@@ -1,11 +1,15 @@
 
 def AreaJdg(UlsncSig): #cm
-    if UlsncSig >= 70:
-        AreaJdg = 0 #Area4
-    elif UlsncSig >= 40:
-        AreaJdg = 1 #Area3
-    elif UlsncSig >= 20:
-        AreaJdg = 2 #Area2
+    dist_obj = UlsncSig
+    # フィルタ機能
+    
+
+    if dist_obj >= 70:
+        area_jdg = 0 #Area4
+    elif dist_obj >= 40:
+        area_jdg = 1 #Area3
+    elif dist_obj >= 20:
+        area_jdg = 2 #Area2
     else:
-        AreaJdg = 3 #Area1	
-    return AreaJdg		
+        area_jdg = 3 #Area1	
+    return area_jdg, dist_obj		
